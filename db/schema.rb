6b2441728ec61e4b9b031f4beddcbb3607645c8c
Mike Adeleke <madeleke13@gmail.com>
2014-01-26 02:39:21 +0000

    added ArticleCategory model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140126023348) do
+ActiveRecord::Schema.define(version: 20140126023708) do
+
+  create_table "article_categories", force: true do |t|
+    t.integer  "article_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "articles", force: true do |t|
     t.string   "title"
