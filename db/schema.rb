6817a2d679a5ed89e149dfa38c1f8b7cc19c9db0
Mike Adeleke <madeleke13@gmail.com>
2014-01-27 17:07:18 +0000

    added users resources

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140126204159) do
+ActiveRecord::Schema.define(version: 20140127164729) do
 
   create_table "article_categories", force: true do |t|
     t.integer  "article_id"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 20140126204159) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
